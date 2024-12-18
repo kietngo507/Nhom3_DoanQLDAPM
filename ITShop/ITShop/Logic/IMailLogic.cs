ad33aa759af9cf4e7dab8d8ac4db0d46fe6d7240
Thịnh Nguyễn Phước <phuocthinh210603@gmail.com>
2024-12-18 19:36:31 +0700

    xoacaicu
    Signed-off-by: Thịnh Nguyễn Phước <phuocthinh210603@gmail.com>

--- ITShop/ITShop/Logic/IMailLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿using ITShop.Models;
-namespace ITShop.Logic
-{
-    public interface IMailLogic
-    {
-        Task GoiEmail(MailInfo mailInfo);
-        Task GoiEmailDatHangThanhCong(DatHang datHang, MailInfo mailInfo);
-    }
-}
